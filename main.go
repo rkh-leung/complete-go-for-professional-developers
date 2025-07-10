5a733b8cc43be5ccc63a480487bd52613f0dfb74
rkh-leung <leptanian@gmail.com>
2025-07-10 15:54:44 +1200

    Variables & basic types

--- main.go
+++ main.go
@@ -7,5 +7,32 @@ import (
 // package main can import but can't export anything
 // go build -o output_file build_file
 func main() {
-	fmt.Println("hello world")
+	// Variables
+	var name string = "Format specifier"
+
+	fmt.Printf("%s for string is %%s\n", name)
+
+	num := 2
+	fmt.Printf("Another way to declare a variable is using inference with walrus operator num = %d\n", num)
+	var city string
+	city = "Auckland"
+	fmt.Printf("%s is my city\n", city)
+
+	var book, cup string = "Golang", "fancy cup"
+	fmt.Printf("You can also assign multiple variables with 'var book, cup string = %s, %s'\n", book, cup)
+
+	var (
+		isEmployed bool   = true
+		salary     int    = 50000
+		position   string = "developer"
+	)
+
+	fmt.Printf("Am I employed? %t. My position is a %s, and I earn %d\n", isEmployed, position, salary)
+
+	// zero values
+	var defaultInt int
+	var defaultFloat float64
+	var defaultString string
+	var defaultBool bool
+	fmt.Printf("%d, %f, %s, %t\n", defaultInt, defaultFloat, defaultString, defaultBool)
 }
