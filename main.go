446db5b02a025080c1fb92fd212afba68ba522e9
rkh-leung <leptanian@gmail.com>
2025-07-21 18:24:17 +1200

    Parsing command line flags using std flag library

--- main.go
+++ main.go
@@ -2,26 +2,30 @@ package main
 
 import (
 	"complete-go-for-professional-developers/internal/app"
+	"flag"
 	"fmt"
 	"net/http"
 	"time"
 )
 
 func main() {
+	var port int
+	flag.IntVar(&port, "port", 42069, "go backend server port") // default port
+	flag.Parse()
+
 	app, err := app.NewApplication()
 	if err != nil {
 		panic(err)
 	}
 
-	app.Logger.Println("The app is running!")
-
 	http.HandleFunc("/health", HealthCheck)
 	server := &http.Server{ // server now stores a pointer to http.Server struct
-		Addr:         ":8080",
+		Addr:         fmt.Sprintf(":%d", port),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
+	app.Logger.Printf("The app is running on port %d\n!", port)
 
 	err = server.ListenAndServe()
 	if err != nil {
