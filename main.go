7e4d9ee7fc3f06e8af3626af73542dca5a99f76f
rkh-leung <leptanian@gmail.com>
2025-07-21 16:05:53 +1200

    Creating an HTTP server

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"complete-go-for-professional-developers/internal/app"
+	"fmt"
+	"net/http"
+	"time"
 )
 
 func main() {
@@ -11,4 +14,22 @@ func main() {
 	}
 
 	app.Logger.Println("The app is running!")
+
+	http.HandleFunc("/health", HealthCheck)
+	server := &http.Server{ // server now stores a pointer to http.Server struct
+		Addr:         ":8080",
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	err = server.ListenAndServe()
+	if err != nil {
+		app.Logger.Fatal(err)
+	}
+}
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) { // r contains data from client
+	fmt.Fprintf(w, "Status is available\n")
+
 }
